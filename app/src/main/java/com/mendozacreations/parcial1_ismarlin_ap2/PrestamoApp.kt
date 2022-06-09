d076e59d56c2d026e66870be2a02d1812650d862
Ismarlynmh <ismarlyn123@gmail.com>
2022-06-08 22:10:21 -0300

    PrestamoApp.kt creada

---
+++ app/src/main/java/com/mendozacreations/parcial1_ismarlin_ap2/PrestamoApp.kt
@@ -0,0 +1,8 @@
+package com.mendozacreations.parcial1_ismarlin_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PrestamoApp : Application() {
+}
