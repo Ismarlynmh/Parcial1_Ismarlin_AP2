e9f24a81df9df7015340d69f612e5358b35e877d
Ismarlynmh <ismarlyn123@gmail.com>
2022-06-08 22:11:06 -0300

    MainActivity.kt creada

--- app/src/main/java/com/mendozacreations/parcial1_ismarlin_ap2/MainActivity.kt
+++ app/src/main/java/com/mendozacreations/parcial1_ismarlin_ap2/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.mendozacreations.parcial1_ismarlin_ap2
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import com.mendozacreations.parcial1_ismarlin_ap2.utils.Screen
 import com.mendozacreations.parcial1_ismarlin_ap2.ui.theme.Parcial1_Ismarlin_AP2Theme
+import com.mendozacreations.parcial1_ismarlin_ap2.ui.Prestamos.ConsultaPrestamoScreen
+import com.mendozacreations.parcial1_ismarlin_ap2.ui.Prestamos.RegistroPrestamoScreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,14 +36,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    val navHostController = rememberNavController()
+
+    NavHost(navController = navHostController, startDestination = Screen.ConsultaScreen.route){
+        composable(Screen.ConsultaScreen.route){
+            ConsultaPrestamoScreen(navHostController = navHostController)
+        }
+        composable(Screen.RegistroScreen.route){
+            RegistroPrestamoScreen(navHostController = navHostController)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Parcial1_Ismarlin_AP2Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
+
