f4fedc28226585b69b2798ebf09b3f5ea36b4dcd
Ismarlynmh <ismarlyn123@gmail.com>
2022-06-17 12:36:13 -0300

    Arreglando proyecto

--- app/src/main/java/com/mendozacreations/parcial1_ismarlin_ap2/ui/Prestamos/RegistroPrestamoScreen.kt
+++ app/src/main/java/com/mendozacreations/parcial1_ismarlin_ap2/ui/Prestamos/RegistroPrestamoScreen.kt
@@ -1,44 +1,28 @@
 package com.mendozacreations.parcial1_ismarlin_ap2.ui.Prestamos
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 
 @Composable
-fun RegistroPrestamoScreen(
-    navHostController: NavHostController,
-    viewModel: PrestamoViewModel = hiltViewModel()
-) {
-    var deudorError by remember {
-        mutableStateOf(false)
-    }
-    var conceptoError by remember {
-        mutableStateOf(false)
-    }
-    var montoError by remember {
-        mutableStateOf(false)
-    }
-
+fun RegistroPrestamoScreen(navHostController: NavHostController, viewModel: PrestamoViewModel = hiltViewModel()
+){
+    val context = LocalContext.current
     Scaffold(
-        topBar= {
-            TopAppBar(
-                title = { Text(text = "Registro de Prestamos") }
-            )
-        },
+        topBar = { TopAppBar(title = { Text(text = "Registro") }) }
     ) {
-        Column(
-            modifier = Modifier.padding(8.dp)
-        ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+
             OutlinedTextField(
                 value = viewModel.deudor,
                 onValueChange = {viewModel.deudor = it},
@@ -50,8 +34,7 @@ fun RegistroPrestamoScreen(
                     Icon(
                         imageVector = Icons.Default.Person,
                         contentDescription = null)
-                },
-                isError = deudorError
+                }
             )
             OutlinedTextField(
                 value = viewModel.concepto,
@@ -64,77 +47,35 @@ fun RegistroPrestamoScreen(
                     Icon(
                         imageVector = Icons.Default.Settings,
                         contentDescription = null)
-                },
-                isError = conceptoError
+                }
             )
+
             OutlinedTextField(
-                value = viewModel.monto.toString(),
-                onValueChange = {viewModel.concepto = it},
-                modifier = Modifier.fillMaxWidth(),
-                label = {
-                    Text(text = "Monto")
-                },
+                label = { Text("Monto") },
                 leadingIcon = {
                     Icon(
                         imageVector = Icons.Default.Check,
-                        contentDescription = null)
-                },
-                isError = montoError
+                        contentDescription = null) },
+                value = viewModel.monto,
+                onValueChange = { viewModel.monto = it },
+                modifier = Modifier.fillMaxWidth()
             )
-            //DEUDOR
-            val assistiveElementText = if (deudorError) "Error: Obligatorio" else "*Obligatorio"
-            val assistiveElementColor = if (deudorError) {
-                MaterialTheme.colors.error
-            } else {
-                MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
-            }
 
-            //CONCEPTO
-            val conceptoElementText = if (conceptoError) "Error: Obligatorio" else "*Obligatorio"
-            val conceptoElementColor = if (conceptoError) {
-                MaterialTheme.colors.error
-            } else {
-                MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
-            }
+            Button(
+                onClick = {
+                    if (validate(viewModel.monto)){
+                        viewModel.Guardar()
+                        navHostController.navigateUp()
+                    }else{
+                        Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 15.dp)
+            ) {
+                Text("Guardar ")
 
-            //MONTO
-            val montoElementText = if (montoError) "Error: Obligatorio" else "*Obligatorio"
-            val montoElementColor = if (montoError) {
-                MaterialTheme.colors.error
-            } else {
-                MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
-            }
-            //MONTO
-            val monto1ElementText = if (viewModel.monto < "0") "Error: No puede ser menor que cero" else "*Obligatorio"
-            val monto1ElementColor = if (viewModel.monto < "0") {
-                MaterialTheme.colors.error
-            } else {
-                MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
-            }
-
-
-            OutlinedButton(onClick = {
-                if(!deudorError){
-                    deudorError = viewModel.deudor.isBlank()
-                }
-                else if(!conceptoError){
-                    conceptoError = viewModel.concepto.isBlank()
-                }
-                else if(!montoError){
-                    montoError = viewModel.monto.isBlank()
-                }
-                else if(viewModel.monto < "0"){
-                    montoError = viewModel.monto.isBlank()
-                }
-                else{
-                    viewModel.Guardar()
-
-                    navHostController.navigateUp()
-                }
-
-
-            }) {
-                Text(text = "Guardar")
             }
         }
     }
